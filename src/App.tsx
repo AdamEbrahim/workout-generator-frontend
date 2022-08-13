48d03309660d81670e57d1b64a2476176a928430
AdamEbrahim <110650531+AdamEbrahim@users.noreply.github.com>
2022-08-13 00:02:14 -0500

    navigation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/navbar';
+import Generator from './pages/Generator';
+import Exercises from './pages/Exercises';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path = '/' element={<Generator />} />
+          <Route path = '/exercises' element={<Exercises />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
